2ba39beeb504589241a9b9e6ae73fdaa20694235
zaybe <zaybe.maliq@student.moringaschool.com>
2026-09-16 17:43:37 +0300

    new title

--- src/components/VehicleForm.jsx
+++ src/components/VehicleForm.jsx
@@ -34,7 +34,7 @@ export default function VehicleForm({
           </select>
         </div>
 
-        {/* Current Mileage Input */}
+        {/* Current MileageInput */}
         <div>
           <label className="block text-xs font-semibold uppercase text-gray-400 mb-2">
             Current Odometer Reading (KM)
