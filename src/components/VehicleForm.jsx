a3b8bc421ce29c7c3c7c7727f71f259b723a7860
zaybe <zaybe.maliq@student.moringaschool.com>
2026-09-16 17:40:28 +0300

    new title

--- src/components/VehicleForm.jsx
+++ src/components/VehicleForm.jsx
@@ -47,7 +47,8 @@ export default function VehicleForm({
           />
         </div>
 
-        {/* Last Service Mileage Input*/}
+        {/* Last Service
+        Mileage Input*/}
         <div>
           <label className="block text-xs font-semibold uppercase text-gray-400 mb-2">
             Last Service Odometer Reading (KM)
