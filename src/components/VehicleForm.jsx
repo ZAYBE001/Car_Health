1aab0db3f07b7f62ded4fc6ef0f5f438a4e6683a
zaybe <zaybe.maliq@student.moringaschool.com>
2026-09-16 17:31:10 +0300

    new title

--- src/components/VehicleForm.jsx
+++ src/components/VehicleForm.jsx
@@ -60,7 +60,7 @@ export default function VehicleForm({
           />
         </div>
 
-        {/*NEW: MechanicNotes Field*/}
+        {/*NEW: Mechanic Notes Field*/}
         <div>
           <label className="block text-xs font-semibold uppercase text-gray-400 mb-2">
             Special Instructions / Driver Notes
